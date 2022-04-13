75d6346a278e28fb78364dc620be20e44d926194
maheshmali6555@gmail.com <maheshmali6555@gmail.com>
2022-04-13 11:25:24 +0530

    first commit

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+	@GetMapping("/employee")
+	public String getUser() {
+		return "welcome to azure mahesh";
+	}
+}
